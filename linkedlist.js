13886167502be35684dd14d25775bb60ab5b6ae8
charlie-daniels <charlied030401@gmail.com>
2022-10-15 23:44:58 +0100

    init: Create template

---
+++ linkedlist.js
@@ -0,0 +1,69 @@
+const LinkedList = () => {
+  const append = (value) => {
+
+  };
+
+  const prepend = (value) => {
+
+  };
+
+  const size = () => {
+
+  };
+
+  const head = () => {
+
+  };
+
+  const tail = () => {
+
+  };
+
+  const at = (index) => {
+
+  };
+
+  const pop = () => {
+
+  };
+
+  const contains = (value) => {
+
+  };
+
+  const find = (value) => {
+
+  };
+
+  const toString = () => {
+
+  };
+
+  const insertAt = (value, index) => {
+
+  };
+
+  const removeAt = (index) => {
+
+  };
+
+  return {
+    append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
+};
+
+const Node = () => ({
+  value: null,
+  nextNode: null,
+});
